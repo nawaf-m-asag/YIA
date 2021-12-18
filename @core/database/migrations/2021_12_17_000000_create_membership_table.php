bea60e149d582c773900ceb3e3c013ede7f67154
Nawaf-m-asag <nm.nawaf2017@gmail.com>
2021-12-18 16:24:41 +0300

    Add membership migrations

---
+++ @core/database/migrations/2021_12_17_000000_create_membership_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membership', function (Blueprint $table) {
+            $table->id();
+            $table->string('membership_name');
+            $table->integer('membership_price')->default(0);
+            $table->integer('membership_point')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membership');
+    }
+}
