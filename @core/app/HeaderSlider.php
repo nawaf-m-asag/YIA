99943a423f96eac98a3336034484ebd10b1f54c0
Nawaf-m-asag <nm.nawaf2017@gmail.com>
2022-04-13 15:33:51 +0300

    image_sm

--- @core/app/HeaderSlider.php
+++ @core/app/HeaderSlider.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class HeaderSlider extends Model
 {
     protected $table = 'header_sliders';
-    protected $fillable = ['title','subtitle','lang','description','btn_01_status','btn_01_text','btn_01_url','image','video_btn_status','video_btn_text','video_btn_url',];
+    protected $fillable = ['title','subtitle','lang','description','btn_01_status','btn_01_text','btn_01_url','image','image_sm','video_btn_status','video_btn_text','video_btn_url',];
 }
